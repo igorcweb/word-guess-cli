c2199c34bb648dc0e085729e24005237ff3ae0fd
Igor Chernyshev <igorchern07@gmail.com>
2018-08-12 18:44:51 -0500

    Letter.js

---
+++ Letter.js
@@ -0,0 +1,24 @@
+const Letter = function(value) {
+  this.value = value.toLowerCase();
+  this.guessed = false;
+};
+
+let letter = new Letter('A');
+
+Letter.prototype.display = function() {
+  let { value, guessed } = this;
+  if (guessed) {
+    return value;
+  }
+  return '_';
+};
+
+Letter.prototype.check = function(guess) {
+  if (this.value === guess) {
+    return (this.guessed = true);
+  }
+  return (this.guessed = false);
+};
+
+// console.log(letter.check('a'));
+// console.log(letter.display());
